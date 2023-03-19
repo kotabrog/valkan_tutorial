cf70103fa3807e9ed65000168e2d43326dcfb00d
kotabrog <suzuki.kota0331@gmail.com>
2023-03-20 00:39:39 +0900

    clear 9

--- vulkan-tutorial/src/main.rs
+++ vulkan-tutorial/src/main.rs
@@ -65,6 +65,7 @@ struct App {
     entry: Entry,
     instance: Instance,
     data: AppData,
+    device: Device,
 }
 
 impl App {
@@ -75,7 +76,8 @@ impl App {
         let mut data = AppData::default();
         let instance = create_instance(window, &entry, &mut data)?;
         pick_physical_device(&instance, &mut data)?;
-        Ok(Self { entry, instance, data })
+        let device = create_logical_device(&instance, &mut data)?;
+        Ok(Self { entry, instance, data, device })
     }
 
     /// Renders a frame for our Vulkan app.
@@ -85,6 +87,8 @@ impl App {
 
     /// Destroys our Vulkan app.
     unsafe fn destroy(&mut self) {
+        self.device.destroy_device(None);
+    
         if VALIDATION_ENABLED {
             self.instance.destroy_debug_utils_messenger_ext(self.data.messenger, None);
         }
@@ -98,7 +102,12 @@ impl App {
 struct AppData {
     messenger: vk::DebugUtilsMessengerEXT,
     physical_device: vk::PhysicalDevice,
+    graphics_queue: vk::Queue,
 }
+
+//================================================
+// Instance
+//================================================
 
 unsafe fn create_instance(
     window: &Window,
@@ -182,9 +191,29 @@ extern "system" fn debug_callback(
     vk::FALSE
 }
 
+//================================================
+// Physical Device
+//================================================
+
 #[derive(Debug, Error)]
 #[error("Missing {0}.")]
 pub struct SuitabilityError(pub &'static str);
+
+unsafe fn pick_physical_device(instance: &Instance, data: &mut AppData) -> Result<()> {
+    for physical_device in instance.enumerate_physical_devices()? {
+        let properties = instance.get_physical_device_properties(physical_device);
+
+        if let Err(error) = check_physical_device(instance, data, physical_device) {
+            warn!("Skipping physical device (`{}`): {}", properties.device_name, error);
+        } else {
+            info!("Selected physical device (`{}`).", properties.device_name);
+            data.physical_device = physical_device;
+            return Ok(());
+        }
+    }
+
+    Err(anyhow!("Failed to find suitable physical device."))
+}
 
 unsafe fn check_physical_device(
     instance: &Instance,
@@ -194,6 +223,46 @@ unsafe fn check_physical_device(
     QueueFamilyIndices::get(instance, data, physical_device)?;
     Ok(())
 }
+
+//================================================
+// Logical Device
+//================================================
+
+unsafe fn create_logical_device(
+    instance: &Instance,
+    data: &mut AppData,
+) -> Result<Device> {
+    let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+
+    let queue_priorities = &[1.0];
+    let queue_info = vk::DeviceQueueCreateInfo::builder()
+        .queue_family_index(indices.graphics)
+        .queue_priorities(queue_priorities);
+
+    let layers = if VALIDATION_ENABLED {
+        vec![VALIDATION_LAYER.as_ptr()]
+    } else {
+        vec![]
+    };
+
+    let features = vk::PhysicalDeviceFeatures::builder();
+
+    let queue_infos = &[queue_info];
+    let info = vk::DeviceCreateInfo::builder()
+        .queue_create_infos(queue_infos)
+        .enabled_layer_names(&layers)
+        .enabled_features(&features);
+
+    let device = instance.create_device(data.physical_device, &info, None)?;
+
+    data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+
+    Ok(device)
+}
+
+//================================================
+// Structs
+//================================================
 
 #[derive(Copy, Clone, Debug)]
 struct QueueFamilyIndices {
@@ -220,20 +289,4 @@ impl QueueFamilyIndices {
             Err(anyhow!(SuitabilityError("Missing required queue families.")))
         }
     }
-}
-
-unsafe fn pick_physical_device(instance: &Instance, data: &mut AppData) -> Result<()> {
-    for physical_device in instance.enumerate_physical_devices()? {
-        let properties = instance.get_physical_device_properties(physical_device);
-
-        if let Err(error) = check_physical_device(instance, data, physical_device) {
-            warn!("Skipping physical device (`{}`): {}", properties.device_name, error);
-        } else {
-            info!("Selected physical device (`{}`).", properties.device_name);
-            data.physical_device = physical_device;
-            return Ok(());
-        }
-    }
-
-    Err(anyhow!("Failed to find suitable physical device."))
 }
